9c99054f89d0d4de39ce45b90e0fc4b96bceb94b
iHamza7 <khanazam3324@gmail.com>
2024-05-11 02:58:20 +0500

    changed

--- strings.java
+++ strings.java
@@ -1,7 +1,7 @@
 public class strings {
     public static void main(String[] args) {
         String firstName = new String("azam");
-        String name = "hamza";
+        String name = "hamza khan";
         System.out.println(name + "  " + firstName);
     }
 }
