361b077b0082e098fb9a4ed7cd1a63f8263cc0aa
iHamza7 <khanazam3324@gmail.com>
2024-05-12 20:30:06 +0500

    changed

---
+++ inheritance.java
@@ -0,0 +1,26 @@
+class Cat {
+
+    public int getlegs(int legs) {
+        return legs;
+    }
+
+    public String getGroup(String group) {
+        return group;
+    }
+}
+
+class Dog extends Cat {
+    public String getFood(String food) {
+        return food;
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        int legs = d.getlegs(2);
+        String group = d.getGroup("dog eats meal");
+
+        System.out.println(legs + " " + group);
+    }
+}
