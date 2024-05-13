3405804758114c0d9ba17de80a90d47b73ef2fa7
iHamza7 <khanazam3324@gmail.com>
2024-05-13 20:31:44 +0500

    changed

---
+++ polymorphism.java
@@ -0,0 +1,24 @@
+
+class A {
+    public void show() {
+        System.out.println("in show");
+    }
+}
+
+class B extends A {
+    @Override
+    public void show() {
+        System.out.println("in a B show");
+        super.show();
+    }
+
+}
+
+public class polymorphism {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();
+
+    }
+
+}
