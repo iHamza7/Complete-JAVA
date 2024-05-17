2831b6794bc087fc35600a2c168639c2484852dd
iHamza7 <khanazam3324@gmail.com>
2024-05-17 23:56:17 +0500

    change

--- polymorphism.java
+++ polymorphism.java
@@ -8,7 +8,7 @@ class A {
 class B extends A {
     @Override
     public void show() {
-        System.out.println("in a show methd");
+        System.out.println("in a show me  kkthd");
         super.show();
     }
 
