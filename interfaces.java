a944c3d0f667196d2baed9477a3f0d6392afbc35
iHamza7 <khanazam3324@gmail.com>
2024-05-18 20:24:58 +0500

    change

---
+++ interfaces.java
@@ -0,0 +1,28 @@
+
+interface A {
+    void show();
+
+    void config();
+}
+
+class B implements A {
+
+    public void config() {
+        System.out.println("in a config");
+
+    }
+
+    public void show() {
+        System.out.println("in a show");
+
+    }
+
+}
+
+public class interfaces {
+    public static void main(String[] args) {
+
+        B obj = new B();
+        obj.config();
+    }
+}
