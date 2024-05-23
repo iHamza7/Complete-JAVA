70c2acceb912faf34c97ae0880d7a41a2b11ba5a
iHamza7 <khanazam3324@gmail.com>
2024-05-23 20:12:43 +0500

    change

---
+++ trycatch.java
@@ -0,0 +1,17 @@
+
+public class trycatch {
+    public static void main(String[] args) {
+        int i = 4;
+        int j = 2;
+
+        try {
+            int result = i / j;
+
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println(
+                    "something went wrong");
+        }
+
+    }
+}
