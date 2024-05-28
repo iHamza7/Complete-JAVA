3d838ef7c8e21b0b45d8f58aa11c1d5aeb295e45
iHamza7 <khanazam3324@gmail.com>
2024-05-28 23:52:03 +0500

    changed

--- assign_operators.java
+++ assign_operators.java
@@ -4,7 +4,7 @@ public class assign_operators {
 
       switch (num) {
          case 1:
-            System.out.println("Monday 1 2 3 4 5 6");
+            System.out.println("Monday 1 2 3 4 5 6 7");
 
             break;
          case 2:
