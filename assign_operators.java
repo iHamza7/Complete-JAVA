5b50aeaf9268b9f885e253981d4fbbbae8ed6b02
iHamza7 <khanazam3324@gmail.com>
2024-05-29 23:50:34 +0500

    changed

--- assign_operators.java
+++ assign_operators.java
@@ -11,7 +11,7 @@ public class assign_operators {
             System.out.println("Tuesday");
             break;
          default:
-            System.out.println("Enter valid Number 2 3 4");
+            System.out.println("Enter valid Number 2 3 4 5");
 
       }
 
