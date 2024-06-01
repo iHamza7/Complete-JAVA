4e73793564eb0d1c70bf4083779faf9507671b8d
iHamza7 <khanazam3324@gmail.com>
2024-06-01 23:43:24 +0500

    changed

--- classes.java
+++ classes.java
@@ -13,7 +13,7 @@ class Calculator {
 
 class Computer {
     public void playMusic() {
-        System.out.println("Music playing 1 2 ");
+        System.out.println("Music playing 1 2 3 ");
     }
 
     public String getMeAPen(int cost) {
