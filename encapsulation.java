506ead2f1a74d159c0115685834dd154f0451bde
iHamza7 <khanazam3324@gmail.com>
2024-06-12 23:07:37 +0500

    changed

--- encapsulation.java
+++ encapsulation.java
@@ -24,7 +24,7 @@ class Boy {
 public class encapsulation {
     public static void main(String[] args) {
         Boy b = new Boy();
-        b.setName("hamza khan");
+        b.setName("hamza khan 1");
         b.setAge(3);
         System.out.println(b.getAge() + " " + b.getName());
     }
