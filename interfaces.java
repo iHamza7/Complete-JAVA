1be02d391825ccda8c54b9c215f7594d89f7d899
iHamza7 <khanazam3324@gmail.com>
2024-06-17 23:06:33 +0500

    changed

--- interfaces.java
+++ interfaces.java
@@ -13,7 +13,7 @@ interface A {
 class B implements A {
 
     public void config() {
-        System.out.println("in a show config  method 1 2 3 4 5 ");
+        System.out.println("in a show config  method 1 2 3 4 5 6 ");
 
     }
 
